199d2e20016e4cc0ef4c5b3413be667b554102d1
RonaldCrack10 <rashidgaming6@gmail.com>
2025-12-15 12:05:33 +0100

    calc_friction(), calc_force() functions

--- Backend/Environment.py
+++ Backend/Environment.py
@@ -1,4 +1,35 @@
-'''
--- Interaction matrix: np.ndarray
+import numpy as np
+from Config.config import FRICTION
 
-'''
+class Environment:
+
+    def __init__(Self):
+        pass
+
+
+    def calc_friction(self, velocity_x, velocity_y) -> tuple[np.ndarray, np.ndarray]:
+        """
+        Calculate friction forces for all particles.
+        Returns:
+            Tuple of numpy arrays representing friction forces in x and y directions.
+
+        friction_x = -FRICTION (coefficient) * velocity_x
+        """
+
+        self.velocity_x = velocity_x
+        self.velocity_y = velocity_y
+
+        friction_x = FRICTION * self.velocity_x
+        friction_y = FRICTION * self.velocity_y
+
+        return friction_x, friction_y
+
+
+    def calc_force(self) -> tuple[np.ndarray, np.ndarray]:
+        """
+        Berechnet die resultierenden Kräfte (Fx_total, Fy_total) auf alle Partikel.
+        Dies beinhaltet Interaktionskräfte und externe Kräfte (z.B. Reibung).
+
+        :return: Ein Tupel (Fx_total, Fy_total) der Gesamtkräfte.
+        """
+        pass
