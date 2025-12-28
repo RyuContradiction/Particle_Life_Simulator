eab20fc28b1a0eec9568ee1a0678e7efa5a2ee52
Michael Adzase <adzasemichael@pm.me>
2025-12-28 15:50:11 +0100

    Logik fuer geschwindigkeit und Reibung abgeschlossen, muessen mit Config.py Konfiguriert werden

--- Backend/Simulation.py
+++ Backend/Simulation.py
@@ -1,12 +1,12 @@
 import numpy as np
-from Backend.particle_system import Particles
+import numba
+from Particles.py import Particles
 from typing import Tuple, Optional
 from Config.py import *
 
 class Environment:
 
     def __init__(self):
-<<<<<<< HEAD
         self._interactionmatrix: np.ndarray = np.array([[0, 1, 2, 3, 4],
                                                        [1, 1, -1, -1, 1],
                                                        [2, -1, 1, -1, 1],
@@ -35,16 +35,16 @@ class Environment:
 		return np.array([neighbours_x, neigbours_y, interactions, indices])
 
 	@numba.jit(nopython = True)
-	def calc_velocity(self, position_x: np.ndarray, position_y: np.ndarray, neigbours_x: np.ndarray, neighbours_y: np.ndarray, interactions: np.ndarray, index: int, indices: np.ndarray ) -> np.ndarray:
-		new_x: np.zeros(neighbour_x.shape[0])
-		new_y: np.zeros(neighbour_y.shape[0])
-		r1: np.ndarray = np.array([position_x, poition_y])
+	def calc_velocity(self, position_x: np.ndarray, position_y: np.ndarray, neighbours_x: np.ndarray, neighbours_y: np.ndarray, interactions: np.ndarray, index: int, indices: np.ndarray ):
+		new_x: np.zeros(neighbours_x.shape[0])
+		new_y: np.zeros(neighbours_y.shape[0])
+		r1: np.ndarray = np.array([position_x, position_y])
 		k: int = 1
 		m1: int = 1
 		m2: int = 1
 		t: float = 0.01
 		for i in range(neighbours_x.shape[0]):
-			r2 = np.array([neighbours_x, neighbours_y ])
+			r2 = np.array([neighbours_x[i], neighbours_y[i]])
 			r = r1 - r2
 			r_abs = np.sqrt((r1[0] - r2[0])**2 + (r1[1] - r2[1])**2)
 			r_norm: np.ndarray = r / r_abs
@@ -64,17 +64,17 @@ class Environment:
 			
 			#Position ändern
 			self._particles.x[index] += self._particles.velocity_x[index] * t
-            self._particles.y[index] += self._particles.velocity_x[index] * t
+            self._particles.y[index] += self._particles.velocity_y[index] * t
 			self._particles.x[indices[i]] += self._particles.velocity_x[indices[i]]  * t
 			self._particles.y[indices[i]] += self._particles.velocity_x[indices[i]]  * t
 
     
-    def diffuse():
+    def diffuse(self):
         pass
        
 
     def get_particles_x(self):
-        return self.particles.x 
+        return self._particles.x 
 
     def get_particles_y(self):
-        return self.particles.y 
+        return self._particles.y 
